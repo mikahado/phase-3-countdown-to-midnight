c393fadac5e4179172c4189e2c6c9124e545f7a3
Michael <mikahado@gmail.com>
2023-02-19 12:48:53 -0500

    commit tree af62b6513c4e38c5c3a0df7591009501f031e9b8

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+
+    x = integer
+
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+
+"HAPPY NEW YEAR!"
+end 
+
+def countdown_with_sleep(integer)
+
+    x = integer
+
+    while x > 0
+        sleep(1)
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
